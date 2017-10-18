e8972c2b01c36e85cee9ffc0a40686cf917ce951
Adel Khial <adel.khial@gmail.com>
2017-10-18 09:24:54 +0100

    Problem 26 + added tests.

--- src/net/projecteuler/problems/_26_LongestRecurringCycle.java
+++ src/net/projecteuler/problems/_26_LongestRecurringCycle.java
@@ -5,32 +5,38 @@ public class _26_LongestRecurringCycle {
     public static void main(String[] args) {
         int max = 0;
 
-        System.out.println(cycleLength(6));
-
-        for(int denominator = 1; denominator <= 1000; denominator++) {
-            System.out.println((double) 1/denominator);
+        for(int denominator = 6; denominator < 1000; denominator++) {
+            if(cycleLength(denominator) > max) {
+                max = denominator;
+            }
         }
+
+        System.out.println(max);
     }
 
-    private static boolean search(int v, int[] array) {
-        boolean found = false;
-        for(int value : array) {
-            found = value == v || found;
+    private static int search(int v, int[] array) {
+        int index = -1;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == v) {
+                index = i;
+                break;
+            }
         }
-        return found;
+        return index;
     }
         
     private static int cycleLength(int denominator) {
         int cycle = 0;
-        int[] remainders = new int[10];
+        int[] remainders = new int[1000];
         int index = 0;
+        int first = 0;
         int remainder = 1%denominator;
-        while(!search(remainder, remainders) && remainder != 0) {
+        while((first = search(remainder, remainders)) == -1 && remainder != 0) {
             cycle++;
             remainders[index++] = remainder;
             remainder *= 10;
             remainder %= denominator;
         }
-        return cycle;
+        return cycle - first;
     }
 }

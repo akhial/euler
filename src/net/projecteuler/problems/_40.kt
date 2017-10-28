5e25355bacb2b13e0f5babf8ae0e7a16b745eb01
Adel Khial <adel.khial@gmail.com>
2017-10-28 21:24:25 +0100

    Problem 40.

---
+++ src/net/projecteuler/problems/_40.kt
@@ -0,0 +1,21 @@
+package net.projecteuler.problems
+
+/**
+ * First build the 1000000 number string using a StringBuilder (take care not to use String concatenation here
+ * as you will get abysmal performance due to constant allocation of new Strings).
+ *
+ * After that simply access indices in the string and multiply them.
+ */
+
+fun main(args: Array<String>) {
+    println(champernowne())
+}
+
+private fun champernowne(): Int {
+    val sb = StringBuilder()
+    for(i in 1..1000000) sb.append(i)
+    val s = sb.toString()
+    var r = 1
+    (0 until 7).map { Math.pow(10.0, it.toDouble()).toInt() }.forEach { r *= s[it - 1].toString().toInt() }
+    return r
+}

fd04c318751a9257303962c034098a8a67c03c1d
Adel Khial <adel.khial@gmail.com>
2017-10-29 14:32:34 +0100

    Problem 47.

---
+++ src/net/projecteuler/api/Factors.kt
@@ -0,0 +1,61 @@
+package net.projecteuler.api
+
+/**
+ * Represents the prime factors of an integer.
+ */
+class Factors(private val factors: ArrayList<Pair<Int, Int>>) {
+    /**
+     * Allows getting the prime factors of an int, the initial prime array contains 1000 primes.
+     * When the argument is larger than the largest prime we increase the prime count by a factor of 1,5.
+     *
+     * Using an eratosthenes sieve will yield better performance.
+     */
+    companion object {
+        val ZERO = Factors(arrayListOf())
+        private var max = 1000
+        private var primes = genPrimes(max)
+
+        private fun genPrimes(len: Int, prev: ArrayList<Int> = arrayListOf(2)): ArrayList<Int> {
+            var q = (prev.last() - 1)/2
+            for(i in 1..len) {
+                do q++ while(!(2*q + 1).isPrime())
+                prev.add(2*q + 1)
+            }
+            return prev
+        }
+
+        fun of(n: Int): Factors {
+            if(n == 0) return ZERO
+
+            val f = arrayListOf<Pair<Int, Int>>()
+            var r = n
+            while(n > primes.last()) {
+                max += max/2
+                primes = genPrimes(max, primes)
+            }
+            var i = 0
+            while(r != 1) {
+                val p = primes[i]
+                var c = 0
+                while(r%p == 0) {
+                    r /= p
+                    c++
+                }
+                if(c != 0) f.add(Pair(p, c))
+                i++
+            }
+            return Factors(f)
+        }
+    }
+
+    /**
+     * Calculates the value of the factors in [factors].
+     */
+    fun value(): Int = factors.map { Math.pow(it.first.toDouble(), it.second.toDouble()).toInt() }
+            .reduce({ t, x -> t*x })
+
+    /**
+     * Returns the number of factors.
+     */
+    fun count(): Int = factors.count()
+}
---
+++ src/net/projecteuler/problems/_47.kt
@@ -0,0 +1,30 @@
+package net.projecteuler.problems
+
+import net.projecteuler.api.Factors
+
+/**
+ * Iterates over n using memoization to save previously calculated factors. Always calculating the
+ * (n+4)th factor in advance. Stops at the first sequence of 4 numbers whose factor count is 4.
+ *
+ * @see [Factors].
+ */
+
+fun main(args: Array<String>) {
+    println(distinctFactors())
+}
+
+private fun distinctFactors(): Int {
+    val factors = arrayListOf<Factors>()
+    var found = false
+    var n = 0
+    (0 until 4).mapTo(factors) { Factors.of(it) }
+    while(!found) {
+        factors.add(Factors.of(n + 4))
+        if(factors[n].count() == 4 &&
+                factors[n + 1].count() == 4 &&
+                factors[n + 2].count() == 4 &&
+                factors[n + 3].count() == 4) found = true
+        n++
+    }
+    return n - 1
+}

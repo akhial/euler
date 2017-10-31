f912f4cbf7e45d5e3d7ed9607fddc2b5a3f03613
Adel Khial <adel.khial@gmail.com>
2017-10-31 19:15:09 +0100

    Problem 52.

---
+++ src/net/projecteuler/problems/_52.kt
@@ -0,0 +1,26 @@
+package net.projecteuler.problems
+
+import net.projecteuler.api.*
+
+/**
+ * For every n, check the products of n and 2 to 6 for whether they are contained in the permutations of n.
+ * Return the first one found.
+ */
+
+fun main(args: Array<String>) {
+    println(permutedMultiples())
+}
+
+private fun permutedMultiples(): Int {
+    for(n in 125874..1000000) {
+        val p = n.digits().permutations()
+        var f = true
+        var m = 2
+        while(m <= 6 && f) {
+            f = p.contains(n*m)
+            m++
+        }
+        if(f) return n
+    }
+    return 0
+}

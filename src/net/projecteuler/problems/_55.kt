b9c9aeeacef5f2c34257c9d2c722fa180d3125a8
Adel Khial <adel.khial@gmail.com>
2017-11-03 15:09:50 +0100

    Problem 56.

--- src/net/projecteuler/problems/_55.kt
+++ src/net/projecteuler/problems/_55.kt
@@ -2,6 +2,12 @@ package net.projecteuler.problems
 
 import net.projecteuler.api.reverse
 
+/**
+ * While we haven't done 50 iterations add to the current n it's reverse and check it it's palindromic,
+ * if not keep going.
+ * Check if we stopped because we found a palindrome or because of iterations.. if its the latter inc count.
+ */
+
 fun main(args: Array<String>) = println(lychrelNumbers())
 
 private fun lychrelNumbers(): Int {
---
+++ src/net/projecteuler/problems/_56.kt
@@ -0,0 +1,21 @@
+package net.projecteuler.problems
+
+import java.math.BigInteger
+
+/**
+ * Use [BigInteger].
+ */
+
+fun main(args: Array<String>) = println(powerfulDigitSum())
+
+private fun powerfulDigitSum(): Int {
+    var max = 0
+    for(a in 1..100) {
+        for(b in 1..100) {
+            val s = BigInteger.valueOf(a.toLong()).pow(b).toString().toCharArray().map { it - '0' }.sum()
+            if(s > max) max = s
+        }
+    }
+    return max
+}
+

14696b7696337795e415349805dcce65afd49100
Adel Khial <adel.khial@gmail.com>
2017-11-03 17:35:09 +0100

    Problem 59.

---
+++ src/net/projecteuler/problems/_59.kt
@@ -0,0 +1,24 @@
+package net.projecteuler.problems
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+/**
+ * For all keys of the form XYZ I decrypt the message, then check for the absence of certain non-english characters
+ * ($, ], | and `). Sum the ASCII values of the resulting characters.
+ */
+
+fun main(args: Array<String>) = println(xorDecryption())
+
+private fun xorDecryption(): Int {
+    val l = Files.lines(Paths.get("res/cipher.txt")).map { it.split(",") }.findFirst().get()
+    for(a in 'a'..'z')
+        for(b in 'a'..'z')
+            for(c in 'a'..'z') {
+                val key = "" + a + b + c
+                val r = l.mapIndexed { i, s -> s.toInt().xor(key[i%3].toInt()) }
+                if(r.none { it == '$'.toInt() || it == ']'.toInt() || it == '|'.toInt() || it == '`'.toInt() })
+                    return r.sum()
+            }
+    return 0
+}

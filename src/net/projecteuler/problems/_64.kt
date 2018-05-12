a11c8b15c5093e31f8ff39f384594242eaf44349
Adel Khial <adel.khial@gmail.com>
2018-05-12 15:17:14 +0100

    Problem 64.

---
+++ src/net/projecteuler/problems/_64.kt
@@ -0,0 +1,38 @@
+package net.projecteuler.problems
+
+import kotlin.math.*
+
+/**
+ * The tricky part was translating paper operations to an actual algorithm all while being wary of floating point
+ * precision problems.
+ * My first approach using doubles only worked for certain n(which is to be expected) the solution was converting the
+ * algorithm to use only integer values.
+ *
+ * This particular implementation is from Daniel Fischer's answer on SO here:
+ * https://stackoverflow.com/questions/12182701/generating-continued-fractions-for-square-roots
+ */
+
+fun main(args: Array<String>) = println(oddPeriodSquareRoots())
+
+private fun oddPeriodSquareRoots(): Int = (2..10000).filter { fractionCycle(it)%2 == 0 }.count()
+
+private fun fractionCycle(n: Int): Int {
+    val r = floor(sqrt(n.toDouble())).toInt()
+    val f = arrayListOf<Int>()
+    f.add(r)
+
+    if(r*r == n) return 1
+
+    var a = r
+    var p = 0
+    var q = 1
+
+    do {
+        p = a*q - p
+        q = (n - p*p)/q
+        a = (r + p)/q
+        f.add(a)
+    } while(q != 1)
+
+    return f.size
+}

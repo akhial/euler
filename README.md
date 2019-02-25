dbd8961e036c1af492498a08d0c570ddcaf0b887
Adel Khial <adel.khial@gmail.com>
2019-02-25 10:10:30 +0100

    Problem 71.

--- README.md
+++ README.md
@@ -4,13 +4,11 @@ My solutions to the problems on Project Euler, in Java and Kotlin. Most of the p
 
 You can find the problems in the [Project Euler Archive](https://projecteuler.net/archives), you can also download a text file containing the first 476 problems [here](https://github.com/davidcorbin/euler-offline).
 
-Please try to solve the problems **on your own** before **not** looking at the solution, and trying some more!
-
-*Seriously*, if you haven't tried please do..
-
 >"e^(iπ)+1 = 0"
 ― Leonhard Euler
 
 >"Project Euler exists to encourage, challenge, and develop the skills and enjoyment of anyone with an interest in the fascinating world of mathematics."
 
-Problems solved: **#1 to #70**, currently solving: **#71**
+![AdelK](https://projecteuler.net/profile/AdelK.png)
+
+Problems solved: **#1 to #71**, currently solving: **#72**
---
+++ src/net/projecteuler/problems/_71.kt
@@ -0,0 +1,31 @@
+package net.projecteuler.problems
+
+import net.projecteuler.api.gcd
+import kotlin.math.abs
+
+/**
+ * The closest fractions (n/d) to 3/7 are the fractions with the largest d and n = trunc(d*3/7).
+ *
+ * Start at d = 1000000 and, going down, Find the largest n for each that satisfies n/d < 3/7 and gcd(n,d) = 1.
+ * The largest fraction is 428570/999997 which is very close to the trivial 428571/1000000 (a non-proper fraction).
+ */
+
+fun main() = println(orderedFractions())
+
+private fun orderedFractions(): Int {
+    val a = 3.0/7
+    var min = Double.MAX_VALUE
+    var minN = 0
+
+    for(d in 1000000 downTo 8) {
+        var n = (a*d).toInt()
+        if(gcd(n, d) != 1) while(gcd(--n, d) != 1);
+        val t = abs(n.toDouble()/d - a)
+        if(t < min) {
+            min = t
+            minN = n
+        }
+    }
+    return minN
+}
+

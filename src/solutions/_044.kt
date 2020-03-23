51d73b28a094271423166690de7900f0ca7c9002
Adel Khial <adel.khial@gmail.com>
2020-03-23 20:55:49 +0100

    Improve problem 44

--- src/solutions/_044.kt
+++ src/solutions/_044.kt
@@ -1,18 +1,25 @@
 package solutions
 
+import kotlin.math.floor
+import kotlin.math.sqrt
+
 /**
- * The first one found when iterating in order happens to be the solution. So I didn't bother adding checks
- * for the minimal distance. It also happens to be in the first 2400 pentagonal numbers.
- *
+ * The minimal distance occurs in the first solution found when iterating in order.
  * Just iterate over the pentagonal numbers while checking the sum and difference for pentagonal-ity.
  */
 
-private const val max = 2400
-private val p = Array(max) { (0.5*(it + 1)*(3*(it + 1) - 1)).toInt() }
+private const val max = 2200
+private val p = IntArray(max) { (it + 1)*(3*(it + 1) - 1)/2 }
 
 fun pentagonNumbers(): Int {
-    var r = 0
-    for(i in 1 until max) (i until max).filter { p.contains(p[i] + p[it]) && p.contains(p[it] - p[i]) }
-            .forEach { r = p[it] - p[i] }
-    return r
+    for(j in 1 until max)
+        for(k in j until max)
+            if(isP(p[j] + p[k]) && isP(p[k] - p[j]))
+                return p[k] - p[j]
+    return 0
+}
+
+private fun isP(n: Int): Boolean {
+    val x = (-5 + sqrt((25 - 12*(2 - 2*n)).toDouble()))/6
+    return x == floor(x)
 }

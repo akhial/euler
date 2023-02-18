bd43a301877d895ef4e1127069c0562a99f69926
Adel Khial <adel.khial@gmail.com>
2023-02-18 14:17:55 +0100

    Problem 2

--- rust/src/_002.rs
+++ rust/src/_002.rs
@@ -1,8 +1,8 @@
 use crate::math::fibonacci_into;
 
 pub fn even_fibonacci_numbers() -> u64 {
-    const MAX_INDEX: usize = 33;
-    let mut d = vec![0; MAX_INDEX + 1];
-    fibonacci_into(MAX_INDEX, &mut d);
+    const MAX: usize = 33;
+    let mut d = vec![0; MAX + 1];
+    fibonacci_into(MAX, &mut d);
     d.into_iter().filter(|x| x % 2 == 0).sum()
 }
